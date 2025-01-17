7e3e2ef63351264f099926c2177d13875ab52840
jbachurski <kbachurski@gmail.com>
2025-01-17 02:06:40 +0100

    i'll just leave this here

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,14 @@
-let () = print_endline "Hello, World!"
+open Fabric
+
+let () =
+  let md =
+    "let g = (x: int => 2*x) in let f = (x: int => x+1) in %print_i32 (g (f 1))"
+    |> Syntax.parse_exn |> Compiler.propagate_types |> Compiler.lift_lambdas
+    |> Assembly.assemble
+  in
+  let valid = Binaryer.C.Module.validate md <> 0 in
+  assert valid;
+  Binaryer.C.Module.optimize md;
+  Binaryer.C.Module.print md;
+  (* This aborts *)
+  Binaryer.C.Module.interpret md
